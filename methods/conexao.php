c9ac0ee1796b5b2e3a2d789e987d782c2a979fb5
William Bierhals <willambierhals>
2023-07-04 11:01:26 -0300

    :sparkles: Add heroku database connection

--- methods/conexao.php
+++ methods/conexao.php
@@ -1,8 +1,15 @@
 <?php 
+    /*
     $servername = "localhost";
     $username = "root";
     $password = "";
     $database = "studentcrud";
+    */
+
+    $servername = "us-cdbr-east-06.cleardb.net";
+    $username = "b94c6a91dd16da";
+    $password = "e2b01ea3";
+    $database = "heroku_384e0c8a4878d43";
 
     // Criando a conexão
     $connection = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
